ad0b8ae31b0c1261e448d456f84952393170bce9
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-08-10 15:27:17 -0700

    Made changes to input, split up functions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Notes = require('./lib/notes.js');
 const minimist = require('minimist');
 const args = minimist(process.argv.slice(2));
 
-
 let userInput = new Input(args);
 
 console.log('my input = ', userInput);
+
