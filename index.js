af48ea193472647c61e6cd4c97ae6802260cf95c
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-08-10 16:19:12 -0700

    Added working notes / updated input / tested index

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const Input = require('./lib/input.js');
-const Notes = require('./lib/notes.js');
+const Note = require('./lib/notes.js');
 
 const minimist = require('minimist');
 const args = minimist(process.argv.slice(2));
 
 let userInput = new Input(args);
 
-console.log('my input = ', userInput);
+let note = new Note(userInput);
 
