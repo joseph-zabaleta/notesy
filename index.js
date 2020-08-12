4d6fe19a917316993aba53e8de76713f0c03a037
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-08-12 12:46:26 -0700

    Added a fix to validation of payload

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 'use strict';
 
+const mongoose = require('mongoose');
+
 const Input = require('./lib/input.js');
 const Note = require('./lib/note.js');
 const help = require('./lib/docs/help.js');
 
-let userInput = new Input();
+// mongoose.connect('mongodb://localhost:27017/notesy', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
 
-console.log('INPUT:', userInput);
-console.log('validation: ', userInput.validate())
+// const db = mongoose.connection;
+// db.on('error', () => {
+//     console.log('Unable to connect to MongoDB. Did you forget to start your service?');
+// });
+// db.on('open', () => {
+//     console.log('Successfully Connected to MongoDB.');
+// });
+
+
+let userInput = new Input();
 
 if (userInput.validate()) {
     new Note(userInput);
@@ -16,6 +29,6 @@ if (userInput.validate()) {
     help[error]();
 };
 
-
+console.log('bottom of index')
 
 
